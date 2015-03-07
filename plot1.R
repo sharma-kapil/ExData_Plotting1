fa843bc3071880d9af80cbd4fc1698334c39807b
Kapil Sharma <kapil2@gmail.com>
2015-03-06 20:42:41 -0500

    Version 2 - 3/6/15 iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii

--- plot1.R
+++ plot1.R
@@ -1,4 +1,5 @@
 #Plot 1 - Exploratory Data Analysis Course  - Week 1 Project
+# NOTE: The data file household_power_consumption.txt should be in the current working directory.
 
 #Uses the sqldf library. Install the package if it not already loaded.
 
@@ -7,13 +8,16 @@ if (!require("sqldf")) {
 }
 require("sqldf")
 
+#Load the dataframe from the file for the specific dates
 fi <- file("household_power_consumption.txt")
 df <- sqldf("select * from fi where Date in ('1/2/2007','2/2/2007')",
             file.format = list(header = TRUE, sep = ";"))
 close(fi)
 
+#Convert the date field to the correct format
 df[,1] <- as.Date (df[,1],format ="%d/%m/%Y")
 
+#Add a Time field to help with the plotting
 df$DT<- strptime (paste(df[,1],df[,2], sep=" "),format ="%Y-%m-%d %H:%M:%S")
 
 
@@ -27,3 +31,7 @@ require("datasets")
 hist (df[,3],freq=TRUE,main="Global Active Power",xlab="Global Active Power (killowatts)",col="orange")
 dev.copy(png,file="plot1.png")
 dev.off()
+
+
+#Clean up
+rm (df,fi)
--- plot4.R
+++ plot4.R
@@ -1,4 +1,5 @@
 #Plot 4 - Exploratory Data Analysis Course - Week 1 Project
+# NOTE: The data file household_power_consumption.txt should be in the current working directory.
 
 #Uses the sqldf library. Install the package if it not already loaded.
 
@@ -7,13 +8,16 @@ if (!require("sqldf")) {
 }
 require("sqldf")
 
+#Load the dataframe from the file for the specific dates
 fi <- file("household_power_consumption.txt")
 df <- sqldf("select * from fi where Date in ('1/2/2007','2/2/2007')",
             file.format = list(header = TRUE, sep = ";"))
 close(fi)
 
+#Convert the date field to the correct format
 df[,1] <- as.Date (df[,1],format ="%d/%m/%Y")
 
+#Add a Time field to help with the plotting
 df$DT<- strptime (paste(df[,1],df[,2], sep=" "),format ="%Y-%m-%d %H:%M:%S")
 
 
@@ -46,3 +50,6 @@ plot(df[,10],df[,4],type="l",xlab="datetime",ylab="Global_reactive_power")
 
 dev.copy(png,file="plot4.png")
 dev.off()
+
+#Clean up
+rm (df,fi)
